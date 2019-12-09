766f9c1746fb00f4773a4f604d4ed7057fbb357f
dhakim87 <dhakimdahakim@gmail.com>
2019-12-09 10:45:10 -0800

    Added trigger function for update_time Demonstrated pattern for adding creation_time, update_time to objects Added creation_time, update_time, account_type to Account

--- model/account.py
+++ model/account.py
@@ -1,9 +1,13 @@
 class Account:
     def __init__(self, account_id, email, auth_provider,
-                 first_name, last_name, address):
+                 first_name, last_name, address, account_type,
+                 creation_time = None, update_time = None):
         self.id = account_id
         self.email = email
         self.auth_provider = auth_provider
         self.first_name = first_name
         self.last_name = last_name
         self.address = address
+        self.account_type = account_type
+        self.creation_time = creation_time
+        self.update_time = update_time
--- repo/account_repo.py
+++ repo/account_repo.py
@@ -7,20 +7,24 @@ class AccountRepo(BaseRepo):
     def __init__(self, transaction):
         super().__init__(transaction)
 
-    _cols = "id, email, auth_provider, first_name, last_name, address"
+    read_cols = "id, email, auth_provider, first_name, last_name, address, " \
+                "account_type, creation_time, update_time"
+    write_cols = "id, email, auth_provider, first_name, last_name, address, " \
+                 "account_type"
 
     @classmethod
     def _row_to_account(cls, r):
-        return Account(r[0], r[1], r[2], r[3], r[4], json.loads(r[5]))
+        return Account(r[0], r[1], r[2], r[3], r[4],
+                       json.loads(r[5]), r[6], r[7], r[8])
 
     @classmethod
     def _account_to_row(cls, a):
-        return (a.id, a.email, a.auth_provider,
-                a.first_name, a.last_name, a.address)
+        return (a.id, a.email, a.auth_provider, a.first_name, a.last_name,
+                json.dumps(a.address), a.account_type)
 
     def get_account(self, account_id):
         with self._transaction.cursor() as cur:
-            cur.execute("SELECT " + AccountRepo._cols + " FROM "
+            cur.execute("SELECT " + AccountRepo.read_cols + " FROM "
                         "account "
                         "WHERE "
                         "account.id = %s", (account_id,))
@@ -34,20 +38,21 @@ class AccountRepo(BaseRepo):
         with self._transaction.cursor() as cur:
             cur.execute("UPDATE account "
                         "SET "
-                        "email = %s, auth_provider = %s "
-                        "first_name = %s, last_name = %s, address = %s "
+                        "email = %s, auth_provider = %s, first_name = %s, "
+                        "last_name = %s, address = %s, account_type = %s "
                         "WHERE "
                         "account.id = %s",
                         (account.email, account.auth_provider,
                          account.first_name, account.last_name,
-                         json.dumps(account.address), account.id)
+                         json.dumps(account.address), account.account_type,
+                         account.id)
                         )
             return cur.rowcount == 1
 
     def create_account(self, account):
         with self._transaction.cursor() as cur:
-            cur.execute("INSERT INTO account (" + AccountRepo._cols + ") "
-                        "VALUES(%s, %s, %s, %s, %s, %s)",
+            cur.execute("INSERT INTO account (" + AccountRepo.write_cols + ") "
+                        "VALUES(%s, %s, %s, %s, %s, %s, %s)",
                         AccountRepo._account_to_row(account))
             return cur.rowcount == 1
 
