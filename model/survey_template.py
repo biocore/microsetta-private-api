a0e4b5be92e7b3715c3cd23ea44456ed84ac8f78
dhakim87 <dhakimdahakim@gmail.com>
2019-12-13 16:33:35 -0800

    Model for displaying possible surveys to take to end user

--- model/survey_template.py
+++ model/survey_template.py
@@ -3,3 +3,12 @@ class SurveyTemplate:
         self.id = survey_id
         self.locale = locale
         self.groups = groups
+
+
+class SurveyTemplateLinkInfo:
+    def __init__(self, survey_template_id, survey_template_title,
+                 survey_template_version, survey_template_type):
+        self.survey_template_id = survey_template_id
+        self.survey_template_title = survey_template_title
+        self.survey_template_version = survey_template_version
+        self.survey_template_type = survey_template_type
