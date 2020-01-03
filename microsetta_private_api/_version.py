dca325c5015226a36b84062287b3799d0a953a9b
Daniel McDonald <danielmcdonald@ucsd.edu>
2020-01-03 12:15:51 -0800

    Address typo spotted by @AmandaBirmingham

--- microsetta_private_api/_version.py
+++ microsetta_private_api/_version.py
@@ -42,7 +42,7 @@ def get_config():
     cfg.VCS = "git"
     cfg.style = "pep440"
     cfg.tag_prefix = ""
-    cfg.parentdir_prefix = "microsetta-priavte-api-"
+    cfg.parentdir_prefix = "microsetta-private-api-"
     cfg.versionfile_source = "microsetta_private_api/_version.py"
     cfg.verbose = False
     return cfg
