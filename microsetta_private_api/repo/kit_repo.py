99623d033a475ea222a3b996c0e40ec78a54b746
dhakim87 <dhakimdahakim@gmail.com>
2020-01-31 17:16:08 -0800

    Ability to make mock kits for unit tests, fixed bug in retrieving kits

--- microsetta_private_api/repo/kit_repo.py
+++ microsetta_private_api/repo/kit_repo.py
@@ -19,10 +19,45 @@ class KitRepo(BaseRepo):
                         "ag_kit.ag_kit_id = ag_kit_barcodes.ag_kit_id "
                         "WHERE "
                         "ag_kit.supplied_kit_id = %s",
-                        supplied_kit_id)
+                        (supplied_kit_id,))
             rows = cur.fetchall()
             if len(rows) == 0:
                 return None
             else:
                 samples = [sample_repo.get_sample(r[1]) for r in rows]
                 return Kit(rows[0][0], samples)
+
+    # NOTE: This should only be used for unit tests!
+    def create_mock_kit(self, supplied_kit_id):
+        with self._transaction.cursor() as cur:
+            kit_id = '77777777-8888-9999-aaaa-bbbbcccccccc'
+            linker_id = '99999999-aaaa-aaaa-aaaa-bbbbcccccccc'
+            barcode = '777777777'
+            cur.execute("INSERT INTO barcode (barcode, status) "
+                        "VALUES(%s, %s)",
+                        (barcode,
+                         'MOCK SAMPLE FOR UNIT TEST'))
+            cur.execute("INSERT INTO ag_kit "
+                        "(ag_kit_id, "
+                        "supplied_kit_id, swabs_per_kit) "
+                        "VALUES(%s, %s, %s)",
+                        (kit_id, supplied_kit_id, 1))
+            cur.execute("INSERT INTO ag_kit_barcodes "
+                        "(ag_kit_barcode_id, ag_kit_id, barcode) "
+                        "VALUES(%s, %s, %s)",
+                        (linker_id, kit_id, barcode))
+
+        return kit_id
+
+    def remove_mock_kit(self):
+        with self._transaction.cursor() as cur:
+            kit_id = '77777777-8888-9999-aaaa-bbbbcccccccc'
+            linker_id = '99999999-aaaa-aaaa-aaaa-bbbbcccccccc'
+            barcode = '777777777'
+            cur.execute("DELETE FROM ag_kit_barcodes "
+                        "WHERE ag_kit_barcode_id=%s",
+                        (linker_id,))
+            cur.execute("DELETE FROM ag_kit WHERE ag_kit_id=%s",
+                        (kit_id,))
+            cur.execute("DELETE FROM barcode WHERE barcode = %s",
+                        (barcode,))
