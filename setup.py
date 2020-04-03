d82b4aff41fc3331b080f739da1595e7de5feb8a
dhakim87 <dhakimdahakim@gmail.com>
2020-04-03 13:07:08 -0700

    Added server_config to stupid egg file

--- setup.py
+++ setup.py
@@ -20,5 +20,11 @@ setup(
     url="https://github.com/biocore/microsetta-private-api",
     description="A RESTful API to support The Microsetta Initiative",
     license='BSD-3-Clause',
-    package_data={'microsetta_private_api': ['db/*.*', 'db/patches/*.sql']},
+    package_data={'microsetta_private_api':
+        [
+            'db/*.*',
+            'db/patches/*.sql',
+            'server_config.json'
+        ]
+    },
 )
