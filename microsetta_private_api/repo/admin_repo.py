ea51c299f9a43676a0309367d74382869de0e4e2
dhakim87 <dhakimdahakim@gmail.com>
2020-04-15 16:43:48 -0700

    Can now pull additional information about a barcode from barcodes.barcode, barcodes.project_barcode and barcodes.project, even when that barcode isn't part of a kit in admin mode

--- microsetta_private_api/repo/admin_repo.py
+++ microsetta_private_api/repo/admin_repo.py
@@ -9,12 +9,12 @@ class AdminRepo(BaseRepo):
         super().__init__(transaction)
 
     def retrieve_diagnostics_by_barcode(self, sample_barcode):
-        with self._transaction.cursor() as cur:
+        with self._transaction.dict_cursor() as cur:
             cur.execute(
                 "SELECT "
-                "ag_kit_barcodes.ag_kit_barcode_id, "
-                "source.id, "
-                "account.id "
+                "ag_kit_barcodes.ag_kit_barcode_id as sample_id, "
+                "source.id as source_id, "
+                "account.id as account_id "
                 "FROM "
                 "ag.ag_kit_barcodes "
                 "LEFT OUTER JOIN "
@@ -29,18 +29,25 @@ class AdminRepo(BaseRepo):
                 "ag_kit_barcodes.barcode = %s",
                 (sample_barcode,))
 
+            print(sample_barcode)
             row = cur.fetchone()
-            if row is None:
-                return None
 
-            sample_id = row[0]
-            source_id = row[1]
-            account_id = row[2]
+            if row is None:
+                sample_id = None
+                source_id = None
+                account_id = None
+            else:
+                sample_id = row["sample_id"]
+                source_id = row["source_id"]
+                account_id = row["account_id"]
 
             account = None
             source = None
-            sample_repo = SampleRepo(self._transaction)
-            sample = sample_repo._get_sample_by_id(sample_id)
+            sample = None
+
+            if sample_id is not None:
+                sample_repo = SampleRepo(self._transaction)
+                sample = sample_repo._get_sample_by_id(sample_id)
 
             if source_id is not None and account_id is not None:
                 account_repo = AccountRepo(self._transaction)
@@ -48,10 +55,24 @@ class AdminRepo(BaseRepo):
                 account = account_repo.get_account(account_id)
                 source = source_repo.get_source(account_id, source_id)
 
+            cur.execute("SELECT * from barcodes.barcode "
+                        "LEFT OUTER JOIN barcodes.project_barcode "
+                        "USING (barcode) "
+                        "LEFT OUTER JOIN barcodes.project "
+                        "USING (project_id) "
+                        "where barcode=%s",
+                        (sample_barcode,))
+            barcode_info = cur.fetchall()
+
+            # How to unwrap a psycopg2 DictRow.  I feel dirty.
+            barcode_info = [{k: v for k, v in x.items()}
+                            for x in barcode_info]  # Get Inceptioned!!
             diagnostic = {
+                "barcode": sample_barcode,
                 "account": account,
                 "source": source,
-                "sample": sample
+                "sample": sample,
+                "barcode_info": barcode_info
             }
 
             return diagnostic
