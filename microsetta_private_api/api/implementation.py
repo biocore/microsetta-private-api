e61fbdd5e6494022ecbaedd70babe65ba3bcb6eb
AmandaBirmingham <abirmingham@ucsd.edu>
2020-04-22 23:09:54 -0700

    modify claim legacy return value to array to match find logins

--- microsetta_private_api/api/implementation.py
+++ microsetta_private_api/api/implementation.py
@@ -89,9 +89,9 @@ def claim_legacy_acct(token_info):
         t.commit()
 
         if acct is None:
-            return jsonify(code=404, message=ACCT_NOT_FOUND_MSG), 404
+            return jsonify([]), 200
 
-        return jsonify(acct.to_api()), 200
+        return jsonify([acct.to_api()]), 200
 
 
 def register_account(body, token_info):
