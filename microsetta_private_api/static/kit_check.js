5f5f2e77f8ce2b7ec4aae45bf2ae6405c8277860
Daniel McDonald <danielmcdonald@ucsd.edu>
2020-05-15 14:52:55 -0700

    Return an anonymous function

--- microsetta_private_api/static/kit_check.js
+++ microsetta_private_api/static/kit_check.js
@@ -1,25 +1,27 @@
 function verify_kit_claim(form_name) {
     // Initialize form validation
     let form_selector = "form[name='" + form_name + "']";
-    $(form_selector).validate({
-        // Specify validation rules
-        rules: {
-            // The key name on the left side is the name attribute
-            // of an input field. Validation rules are defined
-            // on the right side
-            kit_name: {
-                required: true,
-                remote: {
-                    url: "/check_acct_inputs",
-                    type: "post"
+    return function() {
+        $(form_selector).validate({
+            // Specify validation rules
+            rules: {
+                // The key name on the left side is the name attribute
+                // of an input field. Validation rules are defined
+                // on the right side
+                kit_name: {
+                    required: true,
+                    remote: {
+                        url: "/check_acct_inputs",
+                        type: "post"
+                    }
+                },
+                // Make sure the form is submitted to the destination defined
+                // in the "action" attribute of the form when valid
+                submitHandler: function (form) {
+                    form.submit();
                 }
-            },
-            // Make sure the form is submitted to the destination defined
-            // in the "action" attribute of the form when valid
-            submitHandler: function (form) {
-                form.submit();
             }
-        }
-    });
+        });
+    };
 };
 
