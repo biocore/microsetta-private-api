9dd5ed7f3224b508cf85e3290a2bf6ed1d009af2
dhakim87 <dhakimdahakim@gmail.com>
2020-06-12 18:36:37 -0700

    Admin mode now has a system panel to set system wide messages for all users

--- microsetta_private_api/static/input_util.js
+++ microsetta_private_api/static/input_util.js
@@ -8,3 +8,24 @@ function preclude_whitespace(selector)
         });
     });
 }
+
+function replicate_text(input_selector, destination_selector)
+{
+    $(input_selector).bind('input', function(){
+        $(this).val(function(_, v){
+            $(destination_selector).html(v)
+            return v
+        });
+    });
+}
+
+function select_class(input_selector, destination_selector, input_to_class)
+{
+    $(input_selector).change(function(){
+        $(this).val(function(_, v){
+            $(destination_selector).removeClass()
+            $(destination_selector).addClass(input_to_class(v))
+            return v
+        });
+    });
+}
