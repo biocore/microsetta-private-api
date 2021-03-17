40195cf32923dbd07f45b48b482f00b95bf6690e
Oliver-Nizet <olivernizet@att.net>
2021-03-17 16:45:59 -0700

    vioscreen sessions and percent energy

---
+++ microsetta_private_api/model/vioscreen.py
@@ -0,0 +1,64 @@
+import pandas as pd
+
+def normalize_timestamp(timestamp, timezone, normalize_to='US/Pacific'):
+    timezone_lookup = {'Eastern Standard Time': 'US/Eastern',
+                       'Central Standard Time': 'US/Central',
+                       'Pacific Standard Time': 'US/Pacific'}
+
+    if timezone not in timezone_lookup:
+        raise KeyError(f"Unexpected timezone '{timezone}' observed")
+    if timestamp == None:
+        return None
+
+    timestamp = pd.to_datetime(timestamp)
+    timestamp_tz = timestamp.tz_localize(timezone_lookup[timezone])
+    return timestamp_tz.tz_convert(normalize_to)
+
+
+class VioscreenSession:
+    def __init__(self, sessionId, username, protocolId, status, startDate, endDate, cultureCode, created, modified):
+        self.sessionId = sessionId
+        self.username = username
+        self.protocolId = protocolId
+        self.status = status
+        self.startDate = startDate
+        self.endDate = endDate
+        self.cultureCode = cultureCode
+        self.created = created
+        self.modified = modified
+
+    @classmethod
+    def from_vioscreen(cls, sessions_data, users_data):
+        timeZone = users_data['timeZone']
+        startDate = normalize_timestamp(sessions_data['startDate'], timeZone)
+        endDate = normalize_timestamp(sessions_data['endDate'], timeZone)
+        created = normalize_timestamp(sessions_data['created'], timeZone)
+        modified = normalize_timestamp(sessions_data['modified'], timeZone)
+        return cls(sessions_data['sessionId'], sessions_data['username'], sessions_data['protocolId'], sessions_data['status'], startDate, endDate, sessions_data['cultureCode'], created, modified)
+
+class VioscreenPercentEnergyComponent:
+
+    def __init__(self, code, description, short_description, units, amount):
+        self.code = code
+        self.description = description
+        self.short_description = short_description
+        self.units = units
+        self.amount = amount
+
+    @classmethod
+    def from_vioscreen(cls, component):
+        return cls(component['code'], component['description'], component['shortDescription'], component['units'], component['amount'])
+
+
+class VioscreenPercentEnergy:
+
+    def __init__(self, sessionId, energy_components):
+        self.sessionId = sessionId
+        self.energy_components = energy_components
+
+    @classmethod
+    def from_vioscreen(cls, pe_data):
+        sessionId = pe_data['sessionId']
+        energy_components = [VioscreenPercentEnergyComponent.from_vioscreen(
+            component) for component in pe_data['calculations']]
+        return cls(sessionId, energy_components)
