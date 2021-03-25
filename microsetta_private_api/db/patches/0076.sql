18706267e56ebe6559601e9ca4c2c4ec9ce08f54
Daniel McDonald <danielmcdonald@ucsd.edu>
2021-03-24 17:00:31 -0700

    vioscreen_percentenergy table structures

--- microsetta_private_api/db/patches/0076.sql
+++ microsetta_private_api/db/patches/0076.sql
@@ -1,30 +1,39 @@
 -- create tables to match new models for sessions and percent energy data
-DROP TABLE ag.vioscreen_sessions
-DROP TABLE ag.vioscreen_percentenergy
+DROP TABLE IF EXISTS ag.vioscreen_percentenergy;
+DROP TABLE IF EXISTS ag.vioscreen_dietaryscore;
+DROP TABLE IF EXISTS ag.vioscreen_foodconsumption;
+DROP TABLE IF EXISTS ag.vioscreen_eatingpatterns;
+DROP TABLE IF EXISTS ag.vioscreen_mpeds;
+DROP TABLE IF EXISTS ag.vioscreen_percentenergy;
+DROP TABLE IF EXISTS ag.vioscreen_foodcomponents;
 
 CREATE TABLE ag.vioscreen_sessions (
     sessionId varchar PRIMARY KEY,
-    username varchar,
-    protocolId int,
-    status varchar,
-    startDate timestamp,
+    username varchar NOT NULL,
+    protocolId int NOT NULL,
+    status varchar NOT NULL,
+    startDate timestamp NOT NULL,
     endDate timestamp,
-    cultureCode varchar,
-    created timestamp,
-    modified timestamp
+    cultureCode varchar NOT NULL,
+    created timestamp NOT NULL,
+    modified timestamp NOT NULL
+);
+
+CREATE TABLE ag.vioscreen_percentenergy_code (
+    code varchar PRIMARY KEY,
+    description varchar NOT NULL,
+    shortDescription varchar NOT NULL,
+    units varchar NOT NULL
 );
 
 CREATE TABLE ag.vioscreen_percentenergy (
-    sessionId varchar PRIMARY KEY,
-    FOREIGN KEY (sessionId) REFERENCES ag.vioscreen_sessions (sessionId)
+    id uuid DEFAULT uuid_generate_v4() NOT NULL,
+    sessionId varchar NOT NULL,
+    code varchar NOT NULL,
+    amount float NOT NULL,
+    CONSTRAINT vioscreen_percentenergy_pkey PRIMARY KEY ( id ),
+    UNIQUE (sessionId, code),
+    FOREIGN KEY (sessionId) REFERENCES ag.vioscreen_sessions (sessionId),
+    FOREIGN KEY (code) REFERENCES ag.vioscreen_percentenergy_code (code)
 );
 
-CREATE TABLE ag.vioscreen_energycomponent (
-    sessionId varchar PRIMARY KEY,
-    code varchar,
-    description varchar,
-    shortDescription varchar,
-    units varchar,
-    amount float,
-    FOREIGN KEY (sessionId) REFERENCES ag.vioscreen_percentenergy (sessionId)
-);
