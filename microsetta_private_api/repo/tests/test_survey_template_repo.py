057fc9a9ab469e30d0a2bd73a8f4fa7aec89d6eb
Daniel McDonald <danielmcdonald@ucsd.edu>
2021-04-22 14:48:42 -0700

    Dynamically obtain source IDs

--- microsetta_private_api/repo/tests/test_survey_template_repo.py
+++ microsetta_private_api/repo/tests/test_survey_template_repo.py
@@ -1,4 +1,5 @@
 import unittest
+import uuid
 from microsetta_private_api.repo.survey_template_repo import SurveyTemplateRepo
 from microsetta_private_api.repo.transaction import Transaction
 from psycopg2.errors import ForeignKeyViolation
@@ -6,14 +7,30 @@ from psycopg2.errors import ForeignKeyViolation
 
 # test identifiers with a vio ID
 TEST1_ACCOUNT_ID = "80c327ca-a5c7-4c7f-b64b-b219d9ff0b47"
-TEST1_SOURCE_ID = "9590d72f-e183-42ce-8689-6596d2044001"
 TEST1_SAMPLE_ID = "125a7cc5-41ae-44ef-983c-6f1a5213f668"
 TEST1_VIO_ID = "1c689634cea0d11b"
 
+
 # not in registry
 TEST2_ACCOUNT_ID = "735e1689-6976-4d96-9a33-7a19f06602bf"
-TEST2_SOURCE_ID = "05c7140e-644f-4eea-b75f-d96ecc597d4f"
 TEST2_SAMPLE_ID = "7380bb81-7401-45bd-85a0-51001f5f5cf1"
+
+
+# source IDs are not stable in the test database as these
+# IDs are derived from a patch
+with Transaction() as t:
+    with t.cursor() as cur:
+        cur.execute("""SELECT source_id
+                       FROM ag.ag_kit_barcodes
+                       WHERE ag_kit_barcode_id=%s""",
+                    (TEST1_SAMPLE_ID, ))
+        TEST1_SOURCE_ID = cur.fetchone()
+
+        cur.execute("""SELECT source_id
+                       FROM ag.ag_kit_barcodes
+                       WHERE ag_kit_barcode_id=%s""",
+                    (TEST2_SAMPLE_ID, ))
+        TEST2_SOURCE_ID = cur.fetchone()
 
 
 class SurveyTemplateTests(unittest.TestCase):
@@ -49,7 +66,7 @@ class SurveyTemplateTests(unittest.TestCase):
         with Transaction() as t:
             template_repo = SurveyTemplateRepo(t)
             with self.assertRaises(ForeignKeyViolation):
-                template_repo.create_vioscreen_id(TEST2_ACCOUNT_ID[::-1],
+                template_repo.create_vioscreen_id(str(uuid.uuid4()),
                                                   TEST2_SOURCE_ID,
                                                   TEST2_SAMPLE_ID)
 
@@ -57,7 +74,7 @@ class SurveyTemplateTests(unittest.TestCase):
             template_repo = SurveyTemplateRepo(t)
             with self.assertRaises(ForeignKeyViolation):
                 template_repo.create_vioscreen_id(TEST2_ACCOUNT_ID,
-                                                  TEST2_SOURCE_ID[::-1],
+                                                  str(uuid.uuid4()),
                                                   TEST2_SAMPLE_ID)
 
         with Transaction() as t:
@@ -65,7 +82,7 @@ class SurveyTemplateTests(unittest.TestCase):
             with self.assertRaises(KeyError):
                 template_repo.create_vioscreen_id(TEST2_ACCOUNT_ID,
                                                   TEST2_SOURCE_ID,
-                                                  TEST2_SAMPLE_ID[::-1])
+                                                  str(uuid.uuid4()))
 
     def test_get_vioscreen_id_if_exists_valid(self):
         with Transaction() as t:
