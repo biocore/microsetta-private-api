59369df2133b5fc5f8552ab7634dbc0b6bddd73e
Daniel McDonald <danielmcdonald@ucsd.edu>
2021-04-28 17:21:03 -0700

    Get unfinished sessions and by username

--- microsetta_private_api/repo/tests/test_vioscreen_sessions.py
+++ microsetta_private_api/repo/tests/test_vioscreen_sessions.py
@@ -10,6 +10,8 @@ def _to_dt(mon, day, year):
     return datetime(month=mon, day=day, year=year)
 
 
+VIOSCREEN_USERNAME1 = '3379e14164fac0ed'
+
 VIOSCREEN_SESSION = VioscreenSession(sessionId='a session',
                                      username='a user',
                                      protocolId=1234,
@@ -61,7 +63,125 @@ class VioscreenSessions(unittest.TestCase):
             obs = r.get_session('does not exist')
             self.assertEqual(obs, None)
 
+    def test_get_sessions_by_username_exists(self):
+        with Transaction() as t:
+            r = VioscreenSessionRepo(t)
+            obs = r.upsert_session(VIOSCREEN_SESSION)
+            self.assertEqual(obs, True)
+
+            obs = r.get_sessions_by_username(VIOSCREEN_SESSION.username)
+            self.assertEqual(obs, [VIOSCREEN_SESSION, ])
+
+    def test_get_sessions_by_username_multiple(self):
+        to_insert = [VIOSCREEN_SESSION.copy(),
+                     VIOSCREEN_SESSION.copy(),
+                     VIOSCREEN_SESSION.copy()]
+
+        # NOTE: the username is held constant across these records. This
+        # emulates a user haveing multiple FFQ sessions
+        to_insert[0].sessionId = 'session1'
+        to_insert[1].sessionId = 'session2'
+        to_insert[2].sessionId = 'session3'
+
+        to_insert[0].created = _to_dt(1, 1, 1970)
+        to_insert[1].created = _to_dt(1, 1, 1980)
+        to_insert[2].created = _to_dt(1, 1, 1990)
+
+        # the third entry, while started the latest, does not have an enddate
+        # so cannot be complete
+        to_insert[0].endDate = _to_dt(1, 1, 1971)
+        to_insert[1].endDate = _to_dt(1, 1, 1981)
+        to_insert[2].endDate = None
+
+        with Transaction() as t:
+            r = VioscreenSessionRepo(t)
+            for record in to_insert:
+                obs = r.upsert_session(record)
+                self.assertEqual(obs, True)
+
+            obs = r.get_sessions_by_username(VIOSCREEN_SESSION.username)
+            self.assertEqual(obs, to_insert)
+
+    def test_get_sessions_by_username_does_not_exist(self):
+        with Transaction() as t:
+            r = VioscreenSessionRepo(t)
+            obs = r.get_sessions_by_username('does not exist')
+            self.assertEqual(obs, None)
+
+    def test_get_unfinished_sessions(self):
+        with Transaction() as t:
+            r = VioscreenSessionRepo(t)
+            cur = t.cursor()
+            cur.execute("SELECT vio_id FROM ag.vioscreen_registry")
+            exp = {r[0] for r in cur.fetchall()}
+            obs = r.get_unfinished_sessions()
+            self.assertEqual({r.username for r in obs}, exp)
+
+            user1 = VIOSCREEN_SESSION.copy()
+            user1.username = VIOSCREEN_USERNAME1
+            obs = r.upsert_session(user1)
+            self.assertTrue(obs)
+
+            # our base session is still unfinished (no end date)
+            cur.execute("SELECT vio_id FROM ag.vioscreen_registry")
+            exp = {r[0] for r in cur.fetchall()}
+            obs = r.get_unfinished_sessions()
+            self.assertEqual({r.username for r in obs}, exp)
+
+            # set a finished status
+            user1.status = 'Finished'
+            user1.endDate = _to_dt(1, 1, 1971)
+            obs = r.upsert_session(user1)
+            self.assertTrue(obs)
+
+            # our users record is finished so we shouldn't get it back
+            cur.execute("SELECT vio_id FROM ag.vioscreen_registry")
+            exp = {r[0] for r in cur.fetchall()}
+            obs = r.get_unfinished_sessions()
+            self.assertEqual({r.username for r in obs},
+                             exp - {VIOSCREEN_USERNAME1, })
+
+    def test_get_unfinished_sessions_multiple(self):
+        with Transaction() as t:
+            r = VioscreenSessionRepo(t)
+            cur = t.cursor()
+            cur.execute("SELECT vio_id FROM ag.vioscreen_registry")
+            exp = {r[0] for r in cur.fetchall()}
+            obs = r.get_unfinished_sessions()
+            self.assertEqual({r.username for r in obs}, exp)
+
+            sess1 = VIOSCREEN_SESSION.copy()
+            sess1.username = VIOSCREEN_USERNAME1
+            sess2 = VIOSCREEN_SESSION.copy()
+            sess2.username = VIOSCREEN_USERNAME1
+            sess2.sessionId = 'a different sessionid'
+
+            obs = r.upsert_session(sess1)
+            self.assertTrue(obs)
+            obs = r.upsert_session(sess2)
+            self.assertTrue(obs)
+
+            # our sessions are unfinished
+            cur.execute("SELECT vio_id FROM ag.vioscreen_registry")
+            exp = {r[0] for r in cur.fetchall()}
+            obs = r.get_unfinished_sessions()
+            self.assertEqual({r.username for r in obs}, exp)
+
+            # set a finished status
+            sess1.status = 'Finished'
+            sess1.endDate = _to_dt(1, 1, 1971)
+            obs = r.upsert_session(sess1)
+            self.assertTrue(obs)
+
+            # one session is finished, and we only actually understand the
+            # sematics of a single session anyway, so under our current
+            # operating assumptions, this users FFQ is now complete
+            cur.execute("SELECT vio_id FROM ag.vioscreen_registry")
+            exp = {r[0] for r in cur.fetchall()}
+            obs = r.get_unfinished_sessions()
+            self.assertEqual({r.username for r in obs},
+                             exp - {VIOSCREEN_USERNAME1, })
+
 
 if __name__ == '__main__':
     unittest.main()
-
