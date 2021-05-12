64ec9bf42c539c983abe69510a4d9907ba3e88a0
Oliver-Nizet <olivernizet@att.net>
2021-05-12 16:11:42 -0700

    passing tests + unique index on food components

--- microsetta_private_api/db/patches/0080.sql
+++ microsetta_private_api/db/patches/0080.sql
@@ -1,10 +1,10 @@
 CREATE TABLE ag.vioscreen_foodcomponents (
-    id uuid DEFAULT uuid_generate_v4() NOT NULL,
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     sessionId varchar NOT NULL,
     code varchar NOT NULL,
     amount float NOT NULL,
-    CONSTRAINT vioscreen_foodcomponents_pkey PRIMARY KEY ( id ),
     UNIQUE (sessionId, code),
     FOREIGN KEY (sessionId) REFERENCES ag.vioscreen_sessions (sessionId)
 );
 CREATE INDEX vio_foodc_by_sessionid ON ag.vioscreen_foodcomponents(sessionId);
+CREATE UNIQUE INDEX vio_foodc_by_sessionidcode ON ag.vioscreen_foodcomponents(sessionId, code);
