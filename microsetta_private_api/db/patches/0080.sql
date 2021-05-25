3e152cab68696a7344df546e37866479166a7510
dhakim87 <dhakimdahakim@gmail.com>
2021-05-25 10:59:03 -0700

    Switching lowercase_UPPERCASE style matching flask-babel

--- microsetta_private_api/db/patches/0080.sql
+++ microsetta_private_api/db/patches/0080.sql
@@ -1,5 +1,5 @@
 -- language is a reserved keyword in some variants of sql, (but not Postgres)
 -- so we use preferred_language at the DB layer.
 ALTER TABLE account ADD COLUMN preferred_language VARCHAR;
-UPDATE account SET preferred_language='en-US';
+UPDATE account SET preferred_language='en_US';
 ALTER TABLE account ALTER COLUMN preferred_language SET NOT NULL;
