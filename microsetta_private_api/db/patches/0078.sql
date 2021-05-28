f9b1b09b8ee480b0ffeeed09df6b2889f53c91b8
Daniel McDonald <danielmcdonald@ucsd.edu>
2021-05-28 12:29:26 -0700

    move modification of 78 to 81

--- microsetta_private_api/db/patches/0078.sql
+++ microsetta_private_api/db/patches/0078.sql
@@ -3,10 +3,10 @@
 
 UPDATE ag.survey_question
 SET
-american = 'Describe the quality of your bowel movements. Use the chart below as a reference:<br/><img src="/static/img/en_us/bristol_stool.jpg" id="bristol-chart">',
-british = 'Describe the quality of your bowel movements. Use the chart below as a reference:<br/><img src="/static/img/en_us/bristol_stool.jpg" id="bristol-chart">',
-spanish = 'Describa la calidad de la evacuación. Utilice el siguiente cuadro a modo de referencia:<br/><img src="/static/img/es_mx/bristol_stool.jpg" id="bristol-chart">',
-french = 'Veuillez décrire la qualité de vos selles. Vous pouvez utiliser le tableau ci-dessous comme référence:<br/><img src="/static/img/fr_fr/bristol_stool.jpg" id="bristol-chart">',
-chinese = '描述您的排便质量。使用下面的图表作为参考：<br/><img src="/static/img/zh_cn/bristol_stool.jpg" id="bristol-chart">'
+american = 'Describe the quality of your bowel movements. Use the chart below as a reference:<br/><img src="/static/img/en_us/bristol_stool.jpg">',
+british = 'Describe the quality of your bowel movements. Use the chart below as a reference:<br/><img src="/static/img/en_us/bristol_stool.jpg">',
+spanish = 'Describa la calidad de la evacuación. Utilice el siguiente cuadro a modo de referencia:<br/><img src="/static/img/es_mx/bristol_stool.jpg">',
+french = 'Veuillez décrire la qualité de vos selles. Vous pouvez utiliser le tableau ci-dessous comme référence:<br/><img src="/static/img/fr_fr/bristol_stool.jpg">',
+chinese = '描述您的排便质量。使用下面的图表作为参考：<br/><img src="/static/img/zh_cn/bristol_stool.jpg">'
 WHERE
 survey_question_id = 38;
--- microsetta_private_api/db/patches/0081.sql
+++ microsetta_private_api/db/patches/0081.sql
@@ -18,3 +18,14 @@ UPDATE ag.survey_group SET spanish='Surfistas' WHERE group_order=-4;
 UPDATE ag.survey_group SET spanish='Microbioma personal' WHERE group_order=-5;
 UPDATE ag.survey_group SET spanish='Cuestionario COVID19' WHERE group_order=-6;
 UPDATE ag.survey_group SET spanish='Iniciativa Microsetta' WHERE group_order=-1;
+
+-- set a stylizable ID for bristol images
+UPDATE ag.survey_question
+SET
+american = 'Describe the quality of your bowel movements. Use the chart below as a reference:<br/><img src="/static/img/en_us/bristol_stool.jpg" id="bristol-chart">',
+british = 'Describe the quality of your bowel movements. Use the chart below as a reference:<br/><img src="/static/img/en_us/bristol_stool.jpg" id="bristol-chart">',
+spanish = 'Describa la calidad de la evacuación. Utilice el siguiente cuadro a modo de referencia:<br/><img src="/static/img/es_mx/bristol_stool.jpg" id="bristol-chart">',
+french = 'Veuillez décrire la qualité de vos selles. Vous pouvez utiliser le tableau ci-dessous comme référence:<br/><img src="/static/img/fr_fr/bristol_stool.jpg" id="bristol-chart">',
+chinese = '描述您的排便质量。使用下面的图表作为参考：<br/><img src="/static/img/zh_cn/bristol_stool.jpg" id="bristol-chart">'
+WHERE
+survey_question_id = 38;
