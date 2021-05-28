74f0727c6e38842bcb1bb5c763f1f3d00ee6523f
Daniel McDonald <danielmcdonald@ucsd.edu>
2021-05-28 16:09:23 -0700

    Migrate 80 patch content as master already has an 80

--- microsetta_private_api/db/patches/0081.sql
+++ microsetta_private_api/db/patches/0081.sql
@@ -1,3 +1,10 @@
+-- language is a reserved keyword in some variants of sql, (but not Postgres)
+-- so we use preferred_language at the DB layer.
+ALTER TABLE account ADD COLUMN preferred_language VARCHAR;
+UPDATE account SET preferred_language='en_US';
+ALTER TABLE account ALTER COLUMN preferred_language SET NOT NULL;
+
+-- add missing headers for spanish
 ALTER TABLE ag.survey_group ADD COLUMN spanish VARCHAR;
 UPDATE ag.survey_group SET spanish='Información general sobre la dieta' WHERE group_order=0;
 UPDATE ag.survey_group SET spanish='Información general' WHERE group_order=1;
