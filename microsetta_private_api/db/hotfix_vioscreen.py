a2c4bee2e0bf145914c41f0c12bdbec1c277aada
dhakim87 <dhakimdahakim@gmail.com>
2021-06-08 12:11:25 -0700

    Added empty 0082 patch, hotfix vioscreen points to new migration function after merge

--- microsetta_private_api/db/hotfix_vioscreen.py
+++ microsetta_private_api/db/hotfix_vioscreen.py
@@ -2,5 +2,5 @@ from microsetta_private_api.LEGACY.sql_connection import TRN
 from microsetta_private_api.db.migration_support import MigrationSupport
 
 with TRN:
-    MigrationSupport.migrate_76(TRN)
+    MigrationSupport.migrate_82(TRN)
     TRN.rollback()
