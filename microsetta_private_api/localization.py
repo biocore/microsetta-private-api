e00e1d5ab2222ee6301c0f4aec88da76f5dab750
Daniel McDonald <danielmcdonald@ucsd.edu>
2021-08-13 16:18:27 -0700

    Revert normalization

--- microsetta_private_api/localization.py
+++ microsetta_private_api/localization.py
@@ -10,23 +10,7 @@ LANG_NAME_KEY = "lang_name"
 LANG_TAG_KEY = "language_tag"
 
 
-class _LANG_SUPPORT(dict):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def __getitem__(self, language_tag):
-        language_tag = self.normalize(language_tag)
-        return super().__getitem__(language_tag)
-
-    def normalize(self, tag):
-        tag = tag.replace('-', '_')
-        for lang in self:
-            if tag.lower() == lang.lower():
-                return lang
-        raise KeyError(f"Unrecognized language_tag: {tag}")
-
-
-LANG_SUPPORT = _LANG_SUPPORT({
+LANG_SUPPORT = {
     EN_US: {
         NEW_PARTICIPANT_KEY: american_gut._NEW_PARTICIPANT,
         LANG_NAME_KEY: "american"
@@ -39,4 +23,4 @@ LANG_SUPPORT = _LANG_SUPPORT({
         NEW_PARTICIPANT_KEY: spanish_gut._NEW_PARTICIPANT,
         LANG_NAME_KEY: "spanish"
     }
-})
+}
