5d4fc2813c3dd1298e039b6f431da6bdd7ed914c
Daniel McDonald <danielmcdonald@ucsd.edu>
2021-08-13 13:30:43 -0700

    centralize normalization of language tags

--- microsetta_private_api/localization.py
+++ microsetta_private_api/localization.py
@@ -9,7 +9,24 @@ NEW_PARTICIPANT_KEY = "new_participant"
 LANG_NAME_KEY = "lang_name"
 LANG_TAG_KEY = "language_tag"
 
-LANG_SUPPORT = {
+
+class _LANG_SUPPORT(dict):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __getitem__(self, language_tag):
+        language_tag = self.normalize(language_tag)
+        return super().__getitem__(language_tag)
+
+    def normalize(self, tag):
+        tag = tag.replace('-', '_')
+        for lang in self:
+            if tag.lower() == lang.lower():
+                return lang
+        raise KeyError(f"Unrecognized language_tag: {tag}")
+
+
+LANG_SUPPORT = _LANG_SUPPORT({
     EN_US: {
         NEW_PARTICIPANT_KEY: american_gut._NEW_PARTICIPANT,
         LANG_NAME_KEY: "american"
@@ -22,13 +39,4 @@ LANG_SUPPORT = {
         NEW_PARTICIPANT_KEY: spanish_gut._NEW_PARTICIPANT,
         LANG_NAME_KEY: "spanish"
     }
-}
-
-
-def normalize(tag):
-    tag = tag.replace('-', '_')
-    for lang in LANG_SUPPORT:
-        if tag.lower() == lang.lower():
-            return lang
-    raise ValueError(f"Unrecognized language_tag: {tag}")
-
+})
