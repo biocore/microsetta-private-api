87295483d75ce9eae0ae82871a8710450944b9bd
cassidysymons <83246693+cassidysymons@users.noreply.github.com>
2021-08-31 12:22:36 -0700

    Add result_processed field
    Added result_processed field to reflect whether a record was successfully processed - regardless of validity - for the sake of limiting duplicate checks

--- microsetta_private_api/db/patches/0085.sql
+++ microsetta_private_api/db/patches/0085.sql
@@ -8,6 +8,7 @@ source_state varchar,
 source_postal varchar,
 source_country varchar,
 source_url text,
+result_processed boolean,
 result_raw text,
 result_codes varchar,
 result_good boolean,
@@ -22,7 +23,7 @@ result_latitude double precision,
 result_longitude double precision
 );
 
-CREATE INDEX source_address_composite ON ag.melissa_address_queries (source_address_1, source_address_2, source_postal, source_country);
+CREATE INDEX source_address_composite ON ag.melissa_address_queries (source_address_1, source_address_2, source_postal, source_country, result_processed);
 
-CREATE INDEX result_address_composite ON ag.melissa_address_queries (result_address_1, result_address_2, result_postal, result_country);
+CREATE INDEX result_address_composite ON ag.melissa_address_queries (result_address_1, result_address_2, result_postal, result_country, result_processed);
 
