ce059332c073d63a05f8c4c8864f1f934a09c87d
Cassidy Symons <83246693+cassidysymons@users.noreply.github.com>
2021-10-19 12:21:03 -0700

    Change campaign not found behavior

--- microsetta_private_api/api/_campaign.py
+++ microsetta_private_api/api/_campaign.py
@@ -1,3 +1,4 @@
+from flask import jsonify
 from microsetta_private_api.repo.campaign_repo import CampaignRepo
 from microsetta_private_api.repo.transaction import Transaction
 
@@ -7,9 +8,7 @@ def get_campaign_information(campaign_id):
         campaign_repo = CampaignRepo(t)
         campaign = campaign_repo.get_campaign_by_id(campaign_id)
 
-        # we don't want to raise an exception if the campaign isn't found
-        # for signup form, we'll just display default campaign
         if campaign is None:
-            return None, 200
+            return jsonify(code=404, message="Campaign not found"), 404
         else:
             return campaign, 200
