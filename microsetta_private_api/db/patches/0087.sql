53e1d6a73a787fe5e6a2df260ad01ce0d412a08f
Cassidy Symons <83246693+cassidysymons@users.noreply.github.com>
2021-10-19 10:50:10 -0700

    DB Patch for Campaigns & Interested Users
    - Added campaigns_projects table to link campaigns to their associated projects
    - Moved campaigns and interested_users from ag to barcodes
    - Added campaign_id, converted_to_account, and converted_to_account_time to interested_users

--- microsetta_private_api/db/patches/0087.sql
+++ microsetta_private_api/db/patches/0087.sql
@@ -1,35 +1,45 @@
-CREATE TABLE ag.interested_users (
-interested_user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-first_name varchar,
-last_name varchar,
-email varchar,
-phone varchar,
-address_1 varchar,
-address_2 varchar,
-city varchar,
-state varchar,
-postal_code varchar,
-country varchar,
-latitude double precision,
-longitude double precision,
-confirm_consent boolean,
-ip_address varchar,
-creation_time timestamp,
-update_time timestamp,
-address_checked boolean,
-address_valid boolean
+CREATE TABLE barcodes.interested_users (
+    interested_user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    campaign_id uuid NOT NULL,
+    first_name varchar NOT NULL,
+    last_name varchar NOT NULL,
+    email varchar NOT NULL,
+    phone varchar,
+    address_1 varchar,
+    address_2 varchar,
+    city varchar,
+    state varchar,
+    postal_code varchar,
+    country varchar,
+    latitude double precision,
+    longitude double precision,
+    confirm_consent boolean,
+    ip_address varchar,
+    creation_time timestamp,
+    update_time timestamp,
+    address_checked boolean,
+    address_valid boolean,
+    converted_to_account boolean,
+    converted_to_account_time timestamp
 );
 
-CREATE TABLE ag.campaigns (
-campaign_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-title varchar,
-instructions text,
-header_image varchar,
-permitted_countries text,
-language_key varchar,
-accepting_participants boolean,
-associated_projects text,
-language_key_alt varchar,
-title_alt varchar,
-instructions_alt text
+CREATE TABLE barcodes.campaigns (
+    campaign_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    title varchar NOT NULL,
+    instructions text,
+    header_image varchar,
+    permitted_countries text,
+    language_key varchar,
+    accepting_participants boolean,
+    language_key_alt varchar,
+    title_alt varchar,
+    instructions_alt text
+);
+
+CREATE TABLE barcodes.campaigns_projects (
+    campaign_id uuid NOT NULL,
+    project_id bigint NOT NULL,
+    CONSTRAINT campaign_project_pkey PRIMARY KEY ( campaign_id, project_id ),
+    CONSTRAINT fk_campaign_id FOREIGN KEY ( campaign_id ) REFERENCES barcodes.campaigns( campaign_id ),
+    CONSTRAINT fk_project_id FOREIGN KEY ( project_id ) REFERENCES barcodes.project( project_id )
 );
