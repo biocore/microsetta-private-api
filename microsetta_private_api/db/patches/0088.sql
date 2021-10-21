c3eee134a1e5f1580a3dc5d453a651e7ecaf3146
Daniel McDonald <danielmcdonald@ucsd.edu>
2021-10-21 09:30:30 -0700

    An additional alter table

--- microsetta_private_api/db/patches/0088.sql
+++ microsetta_private_api/db/patches/0088.sql
@@ -1,3 +1,8 @@
 -- normalize the datatype of the barcodes field. all other tables 
--- use varchar whereas project_barcodes uses a specific width
+-- use varchar whereas project_barcodes and ag_handout_barcodes use 
+-- a specific width
 ALTER TABLE barcodes.project_barcode ALTER COLUMN barcode TYPE VARCHAR;
+ALTER TABLE ag.ag_handout_barcodes ALTER COLUMN barcode TYPE VARCHAR;
+COMMENT ON TABLE ag.ag_handout_barcodes 
+    IS 'This table is deprecated, but retained to maintain historical data';
+
