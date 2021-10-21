4510f00e1e73995112d015024332a0f92c9a9032
Daniel McDonald <danielmcdonald@ucsd.edu>
2021-10-21 09:31:28 -0700

    An additional comment

--- microsetta_private_api/db/patches/0088.sql
+++ microsetta_private_api/db/patches/0088.sql
@@ -5,4 +5,6 @@ ALTER TABLE barcodes.project_barcode ALTER COLUMN barcode TYPE VARCHAR;
 ALTER TABLE ag.ag_handout_barcodes ALTER COLUMN barcode TYPE VARCHAR;
 COMMENT ON TABLE ag.ag_handout_barcodes 
     IS 'This table is deprecated, but retained to maintain historical data';
+COMMENT ON TABLE ag.ag_handout_kits
+    IS 'This table is deprecated, but retained to maintain historical data';
 
