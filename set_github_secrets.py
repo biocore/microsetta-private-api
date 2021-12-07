05dc1e80333d7ef973cb696b9a30b4121813d757
Daniel McDonald <danielmcdonald@ucsd.edu>
2021-12-06 16:48:31 -0800

    let's look at a non secret secret'

--- set_github_secrets.py
+++ set_github_secrets.py
@@ -7,6 +7,7 @@ if os.environ.get('MFR_URL') is not None:
     print("actually setting gh secrets")
     config = loads(open('microsetta_private_api/server_config.json').read())
 
+    print("fr_url: ", os.environ['FUNDRAZR_TEST_URL'])
     config['myfoodrepo_url'] = os.environ['MFR_URL']
     config['myfoodrepo_key'] = os.environ['MFR_TEST_APIKEY']
     config['fundrazr_key'] = os.environ['FUNDRAZR_TEST_APIKEY']
