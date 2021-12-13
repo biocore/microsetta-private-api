c22b9d52932772e6f18b9b0aa19ebdc9b5440062
Daniel McDonald <danielmcdonald@ucsd.edu>
2021-12-13 14:26:30 -0800

    break to kick ci

--- set_github_secrets.py
+++ set_github_secrets.py
@@ -9,6 +9,9 @@ if os.environ.get('MFR_URL') is not None:
     config['myfoodrepo_key'] = os.environ['MFR_TEST_APIKEY']
     config['fundrazr_key'] = os.environ['FUNDRAZR_TEST_APIKEY']
     config['fundrazr_url'] = os.environ['FUNDRAZR_TEST_URL']
+
+    print(config['fundrazr_url'])
+    raise ValueError()
     config['fundrazr_organization'] = os.environ['FUNDRAZR_TEST_ORG']
 
     with open('microsetta_private_api/server_config.json', 'w') as fp:
