cdc5d9da5757ec157fd51ed039c927e1f39ab3fe
Daniel McDonald <danielmcdonald@ucsd.edu>
2021-12-13 17:55:33 -0800

    try without pytz

--- tzbroken.py
+++ tzbroken.py
@@ -4,4 +4,14 @@ import pytz
 x = pytz.timezone('US/Pacific')
 y = datetime.datetime(2017,5,26,15,30,16)
 z = x.localize(y)
-print(time.mktime(z.timetuple()))
+try:
+    print(time.mktime(z.timetuple()))
+except Exception as e:
+    print(repr(e))
+
+try:
+    print(time.mktime(y.timetuple()))
+except Exception as e:
+    print(repr(e))
+
+raise ValueError("trigger build failure")
