af749c78dc210a131d9df37f41fb23b08bfee1fe
Daniel McDonald <danielmcdonald@ucsd.edu>
2021-12-14 18:04:51 -0800

    r2

--- tzbroken.py
+++ tzbroken.py
@@ -6,13 +6,13 @@ y = datetime.datetime(2017,5,26,15,30,16)
 z = x.localize(y)
 
 # without pytz
-print(time.mktime(y.timetuple()))
+#print(time.mktime(y.timetuple()))
 
-print(y.timetuple())
-print(z.timetuple())
+#print(y.timetuple())
+#print(z.timetuple())
 
 # recommended test from stub42
 print(time.mktime((2017,5,26,15,30,16,4,146,1)))
 
 # with pytz
-print(time.mktime(z.timetuple()))
+#print(time.mktime(z.timetuple()))
