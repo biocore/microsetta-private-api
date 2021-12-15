1298ac95753d8302c77a913634fa4bb6f1e18e84
Daniel McDonald <danielmcdonald@ucsd.edu>
2021-12-15 08:12:40 -0800

    the most reduced

--- tzbroken.py
+++ tzbroken.py
@@ -1,18 +1,2 @@
-import datetime
 import time
-import pytz
-x = pytz.timezone('US/Pacific')
-y = datetime.datetime(2017,5,26,15,30,16)
-z = x.localize(y)
-
-# without pytz
-#print(time.mktime(y.timetuple()))
-
-#print(y.timetuple())
-#print(z.timetuple())
-
-# recommended test from stub42
 print(time.mktime((2017,5,26,15,30,16,4,146,1)))
-
-# with pytz
-#print(time.mktime(z.timetuple()))
