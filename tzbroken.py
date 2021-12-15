ebc5a42da0f24a47856f65d86571beee04aa7da2
Daniel McDonald <danielmcdonald@ucsd.edu>
2021-12-14 18:03:10 -0800

    test req from @stub42

--- tzbroken.py
+++ tzbroken.py
@@ -8,5 +8,11 @@ z = x.localize(y)
 # without pytz
 print(time.mktime(y.timetuple()))
 
+print(y.timetuple())
+print(z.timetuple())
+
+# recommended test from stub42
+print(time.mktime((2017,5,26,15,30,16,4,146,1)))
+
 # with pytz
 print(time.mktime(z.timetuple()))
