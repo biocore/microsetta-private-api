9ae81392cabbeb9a4510c4c7371f9f76681ecfdd
Daniel McDonald <danielmcdonald@ucsd.edu>
2022-01-14 15:05:25 -0800

    Allow a null qiita client

--- microsetta_private_api/qiita.py
+++ microsetta_private_api/qiita.py
@@ -1,9 +1,18 @@
 from microsetta_private_api.config_manager import SERVER_CONFIG
-from qiita_client import QiitaClient
+from qiita_client import QiitaClient, NotFoundError
 
 
-qclient = QiitaClient(
-    SERVER_CONFIG["qiita_endpoint"],
-    SERVER_CONFIG["qiita_client_id"],
-    SERVER_CONFIG["qiita_client_secret"]
-)
+if not SERVER_CONFIG['qiita_endpoint']:
+    class _mock:
+        def get(self, *args, **kwargs):
+            raise NotFoundError("No qiita client")
+
+        def post(self, *args, **kwargs):
+            raise NotFoundError("No qiita client")
+    qclient = _mock()
+else:
+    qclient = QiitaClient(
+        SERVER_CONFIG["qiita_endpoint"],
+        SERVER_CONFIG["qiita_client_id"],
+        SERVER_CONFIG["qiita_client_secret"]
+    )
