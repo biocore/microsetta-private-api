978b9a2b88caf487d57ccba56699f2c8fc32619b
Daniel McDonald <danielmcdonald@ucsd.edu>
2022-03-01 11:41:53 -0800

    set expiration for subjects in myfoodrepo

--- microsetta_private_api/util/myfoodrepo.py
+++ microsetta_private_api/util/myfoodrepo.py
@@ -1,3 +1,6 @@
+import datetime
+import pytz
+
 from microsetta_private_api.config_manager import SERVER_CONFIG
 from microsetta_private_api.client.myfoodrepo import MFRClient
 
@@ -8,10 +11,19 @@ def gen_survey_url(mfr_id):
 
 
 def create_subj():
+    # WARNING
+    # This creates a subject *remotely* but does not associate the
+    # ID with a source
+
     client = MFRClient(SERVER_CONFIG["myfoodrepo_study"])
+    participation_length = SERVER_CONFIG["myfoodrepo_participation_days"]
     cohort = client.default_cohort
 
-    # should we set the subject to expire after a given time period?
     subj = client.create_subj(cohort)
+    subj_id = subj.data.subject.key
 
-    return subj.data.subject.key
+    now = datetime.datetime.now(pytz.utc)
+    expire_at = now + datetime.timedelta(days=participation_length)
+    client.update_subj(cohort, subj_id, expire_at)
+
+    return subj_id
---
+++ microsetta_private_api/util/tests/test_myfoodrepo.py
@@ -0,0 +1,31 @@
+import unittest
+from dateutil import parser
+import pytz
+import datetime
+
+from microsetta_private_api.config_manager import SERVER_CONFIG
+from microsetta_private_api.util.myfoodrepo import create_subj
+from microsetta_private_api.client.myfoodrepo import MFRClient
+
+
+class MyFoodRepoTests(unittest.TestCase):
+    def test_create_subj(self):
+        client = MFRClient(SERVER_CONFIG["myfoodrepo_study"])
+        exp_delta = SERVER_CONFIG["myfoodrepo_participation_days"]
+
+        subj_id = create_subj()
+
+        obs = client.read_subj(client.default_cohort, subj_id)
+
+        obs_date = obs.data.subject.expired_at
+        obs_date = parser.parse(obs_date)
+
+        now = datetime.datetime.now(pytz.utc)
+
+        # allow for a 1 minute time between when we set expiration and our test
+        self.assertEqual((obs_date - now).days, exp_delta - 1)
+        self.assertGreater((obs_date - now).seconds, 24 * 59 * 60)
+
+
+if __name__ == '__main__':
+    unittest.main()
