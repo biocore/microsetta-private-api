3a3ed556c0b39d5fa50f81102c2b14e8b72544a4
Don Gauthier <dgauthier@health.ucsd.edu>
2022-05-20 10:14:01 -0700

    cleaning order of polyphenol gen_survey_url parameters

--- microsetta_private_api/api/_survey.py
+++ microsetta_private_api/api/_survey.py
@@ -130,7 +130,8 @@ def _remote_survey_url_pffqsurvey(transaction, account_id, source_id,
 
     if pffq_id is None:
         pffq_id = st_repo.create_pffqsurvey_entry(account_id, source_id)
-    pffq_url = polyphenol.gen_survey_url(pffq_id, language_tag)
+    pffq_url = polyphenol.gen_survey_url(pffq_id, account_id, source_id,
+                                         language_tag)
     st_repo.update_url_pffqsurvey_registry(pffq_url, pffq_id)
     st_repo.set_pffqsurvey_id_into_ag_login_surveys(account_id, source_id,
                                                     pffq_id)
