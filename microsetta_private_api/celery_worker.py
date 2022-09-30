9371ae48f736feca05f0a93267800396701839d5
Cassidy Symons <csymons@eng.ucsd.edu>
2022-09-30 08:37:09 -0700

    Pausing jobs to test on staging

--- microsetta_private_api/celery_worker.py
+++ microsetta_private_api/celery_worker.py
@@ -2,8 +2,10 @@ from microsetta_private_api.server import app
 from microsetta_private_api.celery_utils import celery, init_celery
 from microsetta_private_api.util.vioscreen import refresh_headers
 from microsetta_private_api.admin.daklapack_polling import poll_dak_orders
+from microsetta_private_api.tasks import update_qiita_metadata
 init_celery(celery, app.app)
 
 # Run any celery tasks that require initialization on worker start
-refresh_headers.delay()  # Initialize the vioscreen task with a token
-poll_dak_orders.delay()  # check for orders
+#refresh_headers.delay()  # Initialize the vioscreen task with a token
+#poll_dak_orders.delay()  # check for orders
+update_qiita_metadata.delay()
