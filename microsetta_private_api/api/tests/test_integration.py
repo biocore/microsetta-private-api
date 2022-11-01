07a920e0b1c67a1c747ea14b3789d72fc783341d
ragini7913 <ragini7913@gmail.com>
2022-11-01 15:49:36 -0400

    debug scrub source

--- microsetta_private_api/api/tests/test_integration.py
+++ microsetta_private_api/api/tests/test_integration.py
@@ -1168,10 +1168,30 @@ class IntegrationTests(TestCase):
 
     def test_scrub_source(self):
         """
-            Create a source, add a survey, scrub the source
+            Create a dummy account, new source, add a survey, scrub the source
         """
+        account_id = "aaaaaaaa-bbbb-cccc-dddd-eeeefffffffa"
 
-        account_id = "aaaaaaaa-bbbb-cccc-dddd-eeeeffffffff"
+        with Transaction() as t:
+            accountRepo = AccountRepo(t)
+
+            acc = Account(account_id,
+                          "foo@baz.com",
+                          "standard",
+                          "https://MOCKUNITTEST.com",
+                          "1234ThisIsNotARealSub",
+                          "Dan",
+                          "H",
+                          Address(
+                              "123 Dan Lane",
+                              "Danville",
+                              "CA",
+                              12345,
+                              "US"
+                          ),
+                          "en_US")
+            accountRepo.create_account(acc)
+
         """To add a human source, we need to get consent"""
         resp = self.client.get(
             '/api/accounts/%s/consent?language_tag=en_US&consent_post_url=%s' %
@@ -1257,7 +1277,7 @@ class IntegrationTests(TestCase):
             loc + "?language_tag=en_US",
             headers=MOCK_HEADERS
         )
-        check_response(resp, 204)
+        check_response(resp, 200)
 
     def test_associate_sample_and_survey(self):
         """
