32a7c8274d9b88cbe74f8288f96cb7990ab6a10c
Cassidy Symons <csymons@eng.ucsd.edu>
2023-02-15 14:21:46 -0800

    Test adjustments

--- microsetta_private_api/api/tests/test_api.py
+++ microsetta_private_api/api/tests/test_api.py
@@ -1769,7 +1769,7 @@ class ConsentTests(ApiTests):
             data=json.dumps(consent_data),
             headers=self.dummy_auth)
 
-        self.assertEquals(response.headers, "")
+        self.assertEquals(response.data, "")
         self.assertEquals(201, response.status_code)
 
         # Now let's get that signed consent back
