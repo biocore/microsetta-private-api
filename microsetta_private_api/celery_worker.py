e6fef7c838f1d5b3b4b06e396771169e0d3d5ce4
Cassidy Symons <csymons@eng.ucsd.edu>
2023-08-08 10:28:47 -0700

    Celery tweak

--- microsetta_private_api/celery_worker.py
+++ microsetta_private_api/celery_worker.py
@@ -1,10 +1,12 @@
-from microsetta_private_api.server import app
+from microsetta_private_api.server import build_app
 from microsetta_private_api.celery_utils import celery, init_celery
 from microsetta_private_api.util.vioscreen import refresh_headers
 from microsetta_private_api.admin.daklapack_polling import poll_dak_orders
 from microsetta_private_api.util.fundrazr import get_fundrazr_transactions
 from microsetta_private_api.util.perk_fulfillment import check_shipping_updates
 # from microsetta_private_api.tasks import update_qiita_metadata
+
+app = build_app()
 init_celery(celery, app.app)
 
 # Run any celery tasks that require initialization on worker start
