3dfba2458158d189dc0a263f817f123faee40991
Cassidy Symons <csymons@eng.ucsd.edu>
2023-08-30 09:24:03 -0700

    Activate perk fulfillment task

--- microsetta_private_api/celery_utils.py
+++ microsetta_private_api/celery_utils.py
@@ -45,10 +45,10 @@ def init_celery(celery, app):
            "task": "microsetta_private_api.util.fundrazr.get_fundrazr_transactions",  # noqa
            "schedule": 60 * 60  # every hour
         },
-        # "fulfill_new_transactions": {
-        #    "task": "microsetta_private_api.util.perk_fulfillment.fulfill_new_transactions",  # noqa
-        #    "schedule": 60 * 60  # every hour
-        # },
+        "fulfill_new_transactions": {
+           "task": "microsetta_private_api.util.perk_fulfillment.fulfill_new_transactions",  # noqa
+           "schedule": 60 * 60  # every hour
+        },
         # "fulfill_subscriptions": {
         #    "task": "microsetta_private_api.util.perk_fulfillment.process_subscription_fulfillments",  # noqa
         #    "schedule": 60 * 60 * 24  # every 24 hours
