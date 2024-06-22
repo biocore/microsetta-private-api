461b98d96ed7b9b157d77e0b2893fafb98b4c2e4
ayobi <obrien.aaron.g@gmail.com>
2024-06-21 20:11:52 -0400

    test

--- microsetta_private_api/admin/tests/test_admin_repo.py
+++ microsetta_private_api/admin/tests/test_admin_repo.py
@@ -1433,7 +1433,11 @@ class AdminRepoTests(AdminTests):
                     ('test',)
                 )
                 obs = cur.fetchall()
+                print("OBS", obs)
+                print("Barcode gen", new_barcode)
                 self.assertEqual(obs[0][0], new_barcode[0])
+                self.assertEqual(obs[0][1], new_barcode[0])
+
 
     def test_insert_barcodes_admin_fail_nonexisting_kit(self):
         # test that inserting barcodes to a non-existent kit fails
