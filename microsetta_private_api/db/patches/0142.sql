7e75e08154865c311a36cfeb2ade02a02696007a
ayobi <obrien.aaron.g@gmail.com>
2024-08-28 20:15:14 -0400

    update query if project id exists

--- microsetta_private_api/db/patches/0142.sql
+++ microsetta_private_api/db/patches/0142.sql
@@ -4,8 +4,15 @@
 
 WITH all_observations AS (
     SELECT observation_id FROM barcodes.sample_observations
+),
+add_projects AS (
+    SELECT project_id
+    FROM (VALUES (118), (160)) AS np(project_id)
+    WHERE EXISTS (
+        SELECT 1 FROM barcodes.project WHERE project.project_id = np.project_id
+    )
 )
 INSERT INTO barcodes.sample_observation_project_associations (observation_id, project_id)
 SELECT observation_id, project_id
 FROM all_observations
-CROSS JOIN (VALUES (118), (160)) AS new_projects(project_id);
+CROSS JOIN add_projects;
