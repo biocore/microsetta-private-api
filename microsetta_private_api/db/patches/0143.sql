3bffcc8a84bf96ec2e9de1619446daa3b4f0f4e3
Cassidy Symons <csymons@ucsd.edu>
2025-01-07 15:20:40 -0800

    Add SBI kit article code

---
+++ microsetta_private_api/db/patches/0143.sql
@@ -0,0 +1,2 @@
+-- Add article code for Skin Biome Initiative kit
+INSERT INTO barcodes.daklapack_article (dak_article_code, short_description, detailed_description) VALUES ('3510007E', 'SBI 1matrix tube', 'SBI 1 matrix tube, American English');
