5b5b3f84971db5efbdc7b466fe3a03b5c0c5e4d1
Cassidy Symons <csymons@ucsd.edu>
2025-10-22 10:21:10 -0700

    Revisions

--- microsetta_private_api/db/patches/0148.sql
+++ microsetta_private_api/db/patches/0148.sql
@@ -1,5 +1,5 @@
 -- This update to the consent documents serves two purposes:
--- 1) We need to update the phone number in all consent documents.
+-- 1) We need to update the contaxt information in all consent documents.
 -- 2) We need to add the IRB Protocol number, version, and expiration to all of the consent documents.
 -- NOTE: With this patch, we're going to update our internal version number in the database to match the IRB protocol version.
 
@@ -9,10 +9,52 @@ INSERT INTO ag.consent_documents (consent_type, locale, date_time, consent_conte
     FROM ag.consent_documents
     WHERE version = 2;
 
--- Next, we'll replace the phone number
+-- Next, we'll replace the contact information. It appears there were multiple different sets of verbiage for each language, so we will need to run several different queries to catch all of them.
+-- English version I
 UPDATE ag.consent_documents
-    SET consent_content = REPLACE(consent_content, '858-246-1184', '858-822-2379')
-    WHERE version = 48;
+    SET consent_content = REPLACE(
+                            consent_content,
+                            'If you have questions or research-related problems, you may reach Rob Knight at 858-246-1184 or email our help account: microsetta@ucsd.edu.',
+                            'If you have questions or research-related problems, you may contact:<ul><li>Rob Knight at 858-246-1184 or</li><li>The research team (phone: 858-246-3234, email: microsetta@ucsd.edu)</li></ul>'
+                          )
+    WHERE version = 48 AND locale = 'en_US';
+
+-- English version II
+UPDATE ag.consent_documents
+    SET consent_content = REPLACE(
+                            consent_content,
+                            'If you have questions or research-related problems, you may reach us by emailing our help account microsetta@ucsd.edu or Rob Knight at 858-246-1184.',
+                            'If you have questions or research-related problems, you may contact:<ul><li>Rob Knight at 858-246-1184 or</li><li>The research team (phone: 858-246-3234, email: microsetta@ucsd.edu)</li></ul>'
+                          )
+    WHERE version = 48 AND locale = 'en_US';
+
+-- Spanish version I
+UPDATE ag.consent_documents
+    SET consent_content = REPLACE(
+                            consent_content,
+                            'Si tiene preguntas o problemas relacionados con la investigación, puede comunicarse con Rob Knight al 858-246-1184 o enviar un correo electrónico a nuestra cuenta de ayuda: microsetta@ucsd.edu.',
+                            'Si tiene alguna duda o problemas relacionados con la investigación, puede comunicarse con<ul><li>Rob Knight al 858-246-1184 o con</li><li>El equipo de investigación (teléfono: 858-246-3234, correo electrónico: microsetta@ucsd.edu)</li></ul>'
+                          )
+    WHERE version = 48 AND locale IN ('es_MX', 'es_ES');
+
+-- Spanish version II
+UPDATE ag.consent_documents
+    SET consent_content = REPLACE(
+                            consent_content,
+                            'Si tiene alguna duda o problemas relacionados con la investigación, puede llamar a Rob Knight al 858-246-1184 o enviar un correo electrónico a nuestra cuenta de ayuda: microsetta@ucsd.edu.',
+                            'Si tiene alguna duda o problemas relacionados con la investigación, puede comunicarse con<ul><li>Rob Knight al 858-246-1184 o con</li><li>El equipo de investigación (teléfono: 858-246-3234, correo electrónico: microsetta@ucsd.edu)</li></ul>'
+                          )
+    WHERE version = 48 AND locale IN ('es_MX', 'es_ES');
+
+-- Spanish version III
+UPDATE ag.consent_documents
+    SET consent_content = REPLACE(
+                            consent_content,
+                            'Si tiene alguna duda o problemas relacionados con la investigación, usted puede comunicarse con nosotros enviando un correo electrónico a nuestra cuenta de ayuda microsetta@ucsd.edu o llamando a Rob Knight al 858-246-1184.',
+                            'Si tiene alguna duda o problemas relacionados con la investigación, puede comunicarse con<ul><li>Rob Knight al 858-246-1184 o con</li><li>El equipo de investigación (teléfono: 858-246-3234, correo electrónico: microsetta@ucsd.edu)</li></ul>'
+                          )
+    WHERE version = 48 AND locale IN ('es_MX', 'es_ES');
+
 
 -- Then, we'll tack the IRB protocol number, version, and expiration on. We need to do this in two steps, one for each language we support.
 -- English versions
